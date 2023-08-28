f85268cbcf95c3d5a0d027154c0b47fba2eab62d
Joshy20XX <vze3hw7c@gmail.com>
2023-08-28 13:55:05 -0400

    added end biome
    testing end biome

--- src/main/java/net/mcreator/thenewendupdateforge/init/TheNewEndUpdateForgeModBlocks.java
+++ src/main/java/net/mcreator/thenewendupdateforge/init/TheNewEndUpdateForgeModBlocks.java
@@ -10,10 +10,12 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.level.block.Block;
 
+import net.mcreator.thenewendupdateforge.block.NewEndBiomePortalBlock;
 import net.mcreator.thenewendupdateforge.block.CheeseBlock;
 import net.mcreator.thenewendupdateforge.TheNewEndUpdateForgeMod;
 
 public class TheNewEndUpdateForgeModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, TheNewEndUpdateForgeMod.MODID);
 	public static final RegistryObject<Block> CHEESE = REGISTRY.register("cheese", () -> new CheeseBlock());
+	public static final RegistryObject<Block> NEW_END_BIOME_PORTAL = REGISTRY.register("new_end_biome_portal", () -> new NewEndBiomePortalBlock());
 }
--- src/main/java/net/mcreator/thenewendupdateforge/init/TheNewEndUpdateForgeModItems.java
+++ src/main/java/net/mcreator/thenewendupdateforge/init/TheNewEndUpdateForgeModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.thenewendupdateforge.item.NewEndBiomeItem;
 import net.mcreator.thenewendupdateforge.item.CheeseSliceItem;
 import net.mcreator.thenewendupdateforge.TheNewEndUpdateForgeMod;
 
@@ -20,6 +21,7 @@ public class TheNewEndUpdateForgeModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, TheNewEndUpdateForgeMod.MODID);
 	public static final RegistryObject<Item> CHEESE_SLICE = REGISTRY.register("cheese_slice", () -> new CheeseSliceItem());
 	public static final RegistryObject<Item> CHEESE = block(TheNewEndUpdateForgeModBlocks.CHEESE, CreativeModeTab.TAB_FOOD);
+	public static final RegistryObject<Item> NEW_END_BIOME = REGISTRY.register("new_end_biome", () -> new NewEndBiomeItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
